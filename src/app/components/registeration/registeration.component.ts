1cb93694c671924ba249c3a7ac398ef8dcca45cb
MustafaB3irat <mustafaadwi@gmail.com>
2019-09-21 23:44:21 +0300

    sign up ready

--- src/app/components/registeration/registeration.component.ts
+++ src/app/components/registeration/registeration.component.ts
@@ -14,46 +14,69 @@ export class RegisterationComponent {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-  haveDisease:String;
-  phone:String;
-  id_number:number;
-  name:String
-  cities:String[]
-  bloodTypes:String[]
-  email:String
-  password:String
-  new_user=new User
+  haveDisease: String;
+  phone: String;
+  id_number: number;
+  name: String;
+  cities: String[];
+  bloodTypes: String[];
+  email: String;
+  password: String;
+  new_user: User;
 
-  constructor(private _formBuilder: FormBuilder,private router: Router,private signUpService:SignupService) { }
+  constructor(private _formBuilder: FormBuilder, private router: Router, private signUpService: SignupService) { }
 
   ngOnInit() {
-    
+
+
+    this.new_user = {
+      name: '',
+      email: '',
+      password: '',
+      phone: '',
+      id: '',
+      city: '',
+      bloodType: ''
+    }
+
+
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required],
-      id_numberCheck:['',[Validators.required, Validators.minLength(9)]],
-      phone:['',[Validators.required, Validators.minLength(10)]],
-      email:['',[Validators.required, Validators.email]],
-      password:['',[Validators.required, Validators.minLength(10)]],
+      id_numberCheck: ['', [Validators.required, Validators.minLength(9)]],
+      phone: ['', [Validators.required, Validators.minLength(10)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(10)]],
     });
-    this.cities=['Ramallah','Jenin','Al-Qudis','Nablus']
-    this.bloodTypes=['A+','A-','B+','B-','O+','O-','AB+','AB-']
-    
+    this.cities = ['Ramallah', 'Jenin', 'Al-Qudis', 'Nablus']
+    this.bloodTypes = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']
+
   }
-  denied(str:String) {
-    if(str==="yes"){
+  denied(str: String) {
+    if (str === "yes") {
       this.router.navigate(['/home'])
-    }else{
+    } else {
       return true;
     }
   }
-  onSubmit(user:User){
+  onSubmit(user: User) {
+
+    this.new_user = {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      phone: user.phone,
+      id: user.id,
+      city: user.city,
+      bloodType: user.bloodType
+    }
+
     console.log(user)
     // this.signUpService.register(user);
-    console.log("after >>>>>> "+this.signUpService.register(user))
+    console.log("after >>>>>> " + this.signUpService.register(user))
   }
-  
+
 
 }
